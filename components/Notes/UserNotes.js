ae5b3dd679e9adbcb430a677baab4aa839c45f11
minifiedcode <minifiedcode@gmail.com>
2021-10-13 18:58:16 -0500

    Create note skeleton

---
+++ components/Notes/UserNotes.js
@@ -0,0 +1,12 @@
+export default function Notes() {
+    return (
+        <div>
+            <h2>Note 1</h2>
+            <h2>Note 2</h2>
+            <h2>Note 3</h2>
+            <h2>Note 4</h2>
+            <h2>Note 5</h2>
+            <h2>Note 6</h2>
+        </div>
+    )
+}
