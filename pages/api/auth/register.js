b863f3d77ae5694be1c7a76b829d3603fe6eb4a4
minifiedcode <minifiedcode@gmail.com>
2021-10-16 16:00:23 -0500

    Add async functionality to registration and adds a check to see if user exists.

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,5 +1,8 @@
+import dbConfig from '../../../utilities/mysql'
+const mysql = require('mysql-await')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+let moment = require('moment')
 
 async function hashPassword(password) {
     if (password && password.length >= 8) {
@@ -9,10 +12,56 @@ async function hashPassword(password) {
     } 
 }
 
+async function accountExists(username, email) {
+    let connection = mysql.createConnection(dbConfig)
+
+    connection.on('error', (err) => {
+        console.error(`Connection error ${err.code}`)
+    })
+
+    let usernameSql = 'SELECT username FROM users WHERE username = ?'
+    let emailSql = 'SELECT email FROM users WHERE email = ?'
+    let usernameResult = await connection.awaitQuery(usernameSql, username)
+    let emailResult = await connection.awaitQuery(emailSql, email)
+
+    if(usernameResult[0] || emailResult[0]) return true
+    connection.awaitEnd()
+    return false
+}
+
+async function register(username, email, password) {
+    let connection = mysql.createConnection(dbConfig)
+
+    connection.on('error', (err) => {
+        console.error(`Connection error ${err.code}`)
+    })
+
+    // Formats the date for SQL
+    let currentDate = moment().format('YYYY-MM-DD HH:mm:ss')
+    let values = [username, email, password, currentDate, username, currentDate, username]
+    let sql = `INSERT INTO users (username, email, password, created_at, created_by, updated_at, updated_by)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                `
+    let doesUserExist = await accountExists(username, email)
+    if (!doesUserExist) { 
+        connection.query(sql, [...values], (err, results) => {
+            if (err) return console.log(err)
+            console.log(results)
+        })
+    } else {
+        console.log("You already have an acoount")
+    }
+
+    connection.awaitEnd()
+}
+
 export default function handler(req, res) {
+    let {username, email, password} = req.body
     if (req.method === "POST") {
         if (req.body) {
-            hashPassword(req.body.password).then(data => console.log(data))
+            hashPassword(password).then(data => {
+                register(username, email, data)
+            })
         }
         res.status(200).json({ error: null, post: 'success' })
     } else {
