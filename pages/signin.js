ffa9f183a275c6c9654d8bc0e661ce4f09258978
minifiedcode <minifiedcode@gmail.com>
2021-10-20 23:22:09 -0500

    Rewrite SignIn component

--- pages/signin.js
+++ pages/signin.js
@@ -1,13 +1,51 @@
-import SignInForm from '../components/Authentication/SignInForm'
-import styles from '../components/Authentication/SignInForm.module.scss'
+import styles from '../styles/Authentication/SignInForm.module.scss'
 import Navbar from '../components/Navbar/Navbar'
-
+import { useRouter } from 'next/router'
 
 export default function SignIn() {
+    const router = useRouter()
+
+    const verifyInput = async(e) => {
+        e.preventDefault()
+        const { username, password } = e.target.elements;
+
+        // Submit POST request to authenticate user
+        const res = await fetch('http://localhost:3000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username.value,
+                password: password.value
+            })
+        })
+
+        const data = await res.json()
+
+        if (data.hasAccount) {
+            router.push('http://localhost:3000')
+        }
+    }
+
     return (
         <div>
             <Navbar darkText />
-            <SignInForm />
+
+            <div className={styles.signInContainer}>
+                <form className={styles.signInForm} onSubmit={verifyInput}>
+                    {/* Username */}
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" className={styles.signInInput} required />
+
+                    {/* Password */}
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" className={styles.signInInput} required />
+
+                    {/* Submit buttons */}
+                    <button type="submit" className={`${styles.btnLogin} ${styles.btn}`}>Login</button>
+                </form>
+            </div>
         </div>
     )
 }
