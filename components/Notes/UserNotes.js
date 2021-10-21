c0da920be5427c78ad868930c19a50c808bd35f8
minifiedcode <minifiedcode@gmail.com>
2021-10-20 23:28:04 -0500

    Update Note component to include Navbar

--- components/Notes/UserNotes.js
+++ components/Notes/UserNotes.js
@@ -1,6 +1,11 @@
+import Navbar from '../Navbar/Navbar'
+import styles from './Notes.module.scss'
+
 export default function Notes() {
     return (
-        <div>
+        <div className={styles.notesContainer}>
+            <Navbar darkText />
+
             <h2>Note 1</h2>
             <h2>Note 2</h2>
             <h2>Note 3</h2>
